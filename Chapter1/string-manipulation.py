388d8a59ab6a0e4baafffd61b23bddf3b57c1efa
dqa <retrogamer1888@gmail.com>
2021-05-18 17:20:12 +0530

    Update string-manipulation.py

--- Chapter1/string-manipulation.py
+++ Chapter1/string-manipulation.py
@@ -4,3 +4,5 @@ name = firstName + " " +lastName
 formattedName = name.title()
 finalName = "Hello, " + formattedName + "!"
 print(finalName)
+test = input("Are ya ready for the next examples?Press enter if you are ")
+print("Most Popular Languages:\n\tJavaScript\n\tHTML/CSS\n\tSQL\n\tPython")
